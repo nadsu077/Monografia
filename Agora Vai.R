c8a326c011588931d66cbedd01ed20d31fa08d96
Nadson Ângelo <63198750+nadsu077@users.noreply.github.com>
2022-09-01 18:23:26 -0300

    Update Agora Vai.R

--- Agora Vai.R
+++ Agora Vai.R
@@ -15,6 +15,15 @@ options(scipen = 999)
 # valor de comercialização, percentuais de carregamento (admin, comercial e lucro), 
 # Fator moderador, tipo de contratacao
 
+# Tabela Criada com o nome 'tst', o intuito será fazer uma análise descritiva das informações dos planos de saúde
+# que tem abrangência no estado do Ceará e estão com comercialização ativa, com produtos de nível municipal, 
+# grupo de municípios e estadual.
+
+# Após a análise descritiva será feito a análise preditiva do prêmio puro dos produtos, utilizando modelos de 
+# machine learning, como árvore de decisão, floresta aleatória e extreme gradient boosting. Com o resultado
+# dos preços por faixa etária, será feito a investigação dos valores resultantes para adequação a normativa
+# da ANS referente a prática de preços dos planos de saúde - RN 63/03. Caso o resultado final não esteja em
+# conformidade com a norma vigente, deverá ser feito a adequação dos preços para que se satisfaça a norma.
 # Analise Descritiva ----
 
 # Boxplots do Valor de Comercialização das Mensalidades (VCM) ----
@@ -59,19 +68,16 @@ boxplot(tst$VCM ~ tst$PorteOPS)
 
 # Boxplots do Percentual de Despesa Assistencial ----
 
-# Boxplot do Percentual de Despesa Assistencial
+# Boxplot da Despesa Assistencial
 boxplot(tst$PCT_DESP_ASS)
-boxplot(tst$DESP_ASS)
 
 # Boxplot do Percentual de Despesa Assistencial segundo o Grupo de Modalidade da OPS
 # 1: Autogestão; 2: Coop. Médica; 4: Filantropia; 5: Medicina de Grupo; 7: Seguradora
 boxplot(tst$PCT_DESP_ASS ~ tst$GR_MODALIDADE) 
-boxplot(tst$DESP_ASS ~ tst$GR_MODALIDADE)
 
 # Boxplot do Percentual de Despesa Assistencial segundo o Grupo de Contratação
 # Ind/Fam = 1; Col Emp. = 2, col ad. = 3 e NI = 0
 boxplot(tst$PCT_DESP_ASS ~ tst$GR_CONTRATACAO)
-boxplot(tst$DESP_ASS ~ tst$GR_CONTRATACAO)
 
 # Boxplot do Percentual de Despesa Assistencial segundo a Segmentação Assistencial
 # 1 = "Ambulatorial", 
@@ -83,22 +89,18 @@ boxplot(tst$DESP_ASS ~ tst$GR_CONTRATACAO)
 # 9 = "Hospitalar sem obstetrícia",
 # 12 = "Referência"
 boxplot(tst$PCT_DESP_ASS ~ tst$SGMT_ASSISTENCIAL)
-boxplot(tst$DESP_ASS ~ tst$SGMT_ASSISTENCIAL)
 
 # Boxplot do Percentual de Despesa Assistencial segundo a Abrangência de Cobertura
 # 1-municipal; 2-grupo de municipios; 3-estadual;
 boxplot(tst$PCT_DESP_ASS ~ tst$ABRANGENCIA_COBERTURA)
-boxplot(tst$DESP_ASS ~ tst$ABRANGENCIA_COBERTURA)
 
 # Boxplot do Percentual de Despesa Assistencial segundo o Fator Moderador
 # 1-Ausente; 2-Coparticipação; 3-Franquia; 4-Franquia+Coparticipação
 boxplot(tst$PCT_DESP_ASS ~ tst$FATOR_MODERADOR)
-boxplot(tst$DESP_ASS ~ tst$FATOR_MODERADOR)
 
 # Boxplot do Percentual de Despesa Assistencial segundo o Fator Moderador
 # 1-Coletivo; 2-Individual; 3-Nao se Aplica
 boxplot(tst$PCT_DESP_ASS ~ tst$ACOMODACAO_HOSPITALAR)
-boxplot(tst$DESP_ASS ~ tst$ACOMODACAO_HOSPITALAR)
 
 # Boxplots do Percentual de Demais Carregamentos ----
 
@@ -135,6 +137,42 @@ boxplot(tst$PCT_CARREG ~ tst$FATOR_MODERADOR)
 # Boxplot do Percentual de Demais Carregamentos segundo o Fator Moderador
 # 1-Coletivo; 2-Individual; 3-Nao se Aplica
 boxplot(tst$PCT_CARREG ~ tst$ACOMODACAO_HOSPITALAR)
+
+# Boxplots do Prêmio Puro ----
+
+# Boxplot do Premio Puro
+boxplot(tst$Premio_Puro)
+
+# Boxplot do Premio Puro segundo o Grupo de Modalidade da OPS
+# 1: Autogestão; 2: Coop. Médica; 4: Filantropia; 5: Medicina de Grupo; 7: Seguradora
+boxplot(tst$Premio_Puro ~ tst$GR_MODALIDADE) 
+
+# Boxplot do Premio Puro segundo o Grupo de Contratação
+# Ind/Fam = 1; Col Emp. = 2, col ad. = 3 e NI = 0
+boxplot(tst$Premio_Puro ~ tst$GR_CONTRATACAO)
+
+# Boxplot do Premio Puro segundo a Segmentação Assistencial
+# 1 = "Ambulatorial", 
+# 2 = "Ambulatorial + Hospitalar com obstetrícia", 
+# 3 = "Ambulatorial + Hospitalar com obstetrícia + Odontológico", 
+# 4 = "Ambulatorial + Hospitalar sem obstetrícia", 
+# 5 = "Ambulatorial + Hospitalar sem obstetrícia + Odontológico", 
+# 7 = "Hospitalar com obstetrícia", 
+# 9 = "Hospitalar sem obstetrícia",
+# 12 = "Referência"
+boxplot(tst$Premio_Puro ~ tst$SGMT_ASSISTENCIAL)
+
+# Boxplot do Premio Puro segundo a Abrangência de Cobertura
+# 1-municipal; 2-grupo de municipios; 3-estadual;
+boxplot(tst$Premio_Puro ~ tst$ABRANGENCIA_COBERTURA)
+
+# Boxplot do Premio Puro segundo o Fator Moderador
+# 1-Ausente; 2-Coparticipação; 3-Franquia; 4-Franquia+Coparticipação
+boxplot(tst$Premio_Puro ~ tst$FATOR_MODERADOR)
+
+# Boxplot do Premio Puro segundo o Fator Moderador
+# 1-Coletivo; 2-Individual; 3-Nao se Aplica
+boxplot(tst$Premio_Puro ~ tst$ACOMODACAO_HOSPITALAR)
 
 # Tabelas de Frequência ----
 
@@ -173,23 +211,11 @@ table(tst$ACOMODACAO_HOSPITALAR)
 
 hist(tst$VCM, breaks = 35, main = "Histograma dos Valores de Comercialização")
 
-# Modelagem estatística dos dados ----
+# Modelagem das Informações ----
 
 # Fazendo cópia dos dados
 mydf = tst
 
-'
-# Normalizando as variáveis numéricas
-tst$QTD_MUNICIPIO = scale(tst$QTD_MUNICIPIO)[,1]
-tst$VCM = scale(tst$VCM)[,1]
-tst$PCT_DESP_ASS = scale(tst$PCT_DESP_ASS)[,1]
-tst$PCT_CARREG = scale(tst$PCT_CARREG)[,1]
-tst$PCT_CARREG_ADMIN = scale(tst$PCT_CARREG_ADMIN)[,1]
-tst$PCT_CARREG_COML = scale(tst$PCT_CARREG_COML)[,1]
-tst$PCT_CARREG_LUCRO = scale(tst$PCT_CARREG_LUCRO)[,1]
-tst$QT_BENEF_PLAN = scale(tst$QT_BENEF_PLAN)[,1]
-tst$QT_BENEF_OPS = scale(tst$QT_BENEF_OPS)[,1]
-'
 # Arvore de decisao utilizando o pacote rpart ----
 # link: https://www.r-bloggers.com/2021/04/decision-trees-in-r/
 set.seed(77)
@@ -230,20 +256,20 @@ predito_treino = predict(tree, treino)
 # Histograma da arvore predita e da matriz de treino
 ggplot()+
   geom_histogram(aes(x = predito_treino, col = "Predito")) + 
-  geom_histogram(aes(x = treino$VCM, col = "Treino")) +
+  geom_histogram(aes(x = treino$Premio_Puro, col = "Treino")) +
   scale_fill_manual(values = c(Predito = "blue",
                                Treino = "red"))
 
 par(mfrow = c(1,2))
-boxplot(predito_treino, main = "Boxplot VCM Treino Predito", ylim = c(0,10^4))
-boxplot(treino$VCM, main = "Boxplot VCM Treino", ylim = c(0,10^4))
+boxplot(predito_treino, main = "Boxplot Premio_Puro Treino Predito", ylim = c(0,4000))
+boxplot(treino$Premio_Puro, main = "Boxplot Premio_Puro Treino", ylim = c(0,4000))
 par(mfrow = c(1,1))
 
 # Raiz Quadrática Média do Erro (RMSE)
-sqrt(mean((treino$VCM - predito_treino)^2))
+sqrt(mean((treino$Premio_Puro - predito_treino)^2))
 
 # R-Quadrado (R²)
-(cor(treino$VCM, predito_treino))^2*100
+(cor(treino$Premio_Puro, predito_treino))^2*100
 
 # Prevendo os valores baseado no teste
 predito_teste = predict(tree, teste)
@@ -251,21 +277,21 @@ predito_teste = predict(tree, teste)
 # Histograma da arvore predita e da matriz de teste
 ggplot()+
   geom_histogram(aes(x = predito_teste, col = "Predito")) + 
-  geom_histogram(aes(x = teste$VCM, col = "Teste")) +
+  geom_histogram(aes(x = teste$Premio_Puro, col = "Teste")) +
   scale_fill_manual(values = c(Predito = "blue",
                                Teste = "red"))
 
 # Boxplot
 par(mfrow = c(1,2))
-boxplot(predito_teste, main = "Boxplot VCM Teste Predito", ylim = c(0,10^4))
-boxplot(teste$VCM, main = "Boxplot VCM Teste", ylim = c(0,10^4))
+boxplot(predito_teste, main = "Boxplot Premio_Puro Teste Predito", ylim = c(0,4000))
+boxplot(teste$Premio_Puro, main = "Boxplot Premio_Puro Teste", ylim = c(0,4000))
 par(mfrow = c(1,1))
 
 # Raiz Quadrática Média do Erro (RMSE)
-sqrt(mean((teste$VCM - predito_teste)^2))
+sqrt(mean((teste$Premio_Puro - predito_teste)^2))
 
 # R-Quadrado (R²)
-(cor(teste$VCM, predito_teste))^2
+(cor(teste$Premio_Puro, predito_teste))^2*100
 
 # Random Forest (Pacote randomForest)----
 # link: https://www.r-bloggers.com/2018/01/how-to-implement-random-forests-in-r/
@@ -293,7 +319,7 @@ par(mfrow = c(1,1))
 sqrt(mean((treino$Premio_Puro - predito_treino.rf)^2))
 
 # R²
-(cor(treino$Premio_Puro, predito_treino.rf))^2
+(cor(treino$Premio_Puro, predito_treino.rf))^2*100
 
 # Aplicando o modelo nos dados de teste
 predito_teste.rf = predict(rf, teste)
@@ -376,10 +402,68 @@ cat(
   round(produto_hipo.rf[7] / produto_hipo.rf[1], 2)
 )
 
+par(mfrow = c(3,1))
+plot(tst[,c("CD_FAIXA_ETARIA", "Premio_Puro")])
+plot(produto_hipo.rf)
+plot(produto_hipo.dt)
 
+# As variações entre a 10º e a 7º faixa etária ficou acima da variação entre a 7º e a 1º
+# Aplicando modelo linear para correção e linearização do prêmio puro encontrado
+lm.model = lm(tst$Premio_Puro ~ as.integer(tst$CD_FAIXA_ETARIA))
+lm.model.rf = lm(produto_hipo.rf ~ c(1:10))
+lm.model.dt = lm(produto_hipo.dt ~ c(1:10))
 
+# Resumo estatístico dos modelos
+summary(lm.model)
+summary(lm.model.rf)
+summary(lm.model.dt)
 
+# 
+lm.model.predito = predict(lm.model)
+lm.model.rf.predito = predict(lm.model.rf)
+lm.model.dt.predito = predict(lm.model.dt)
 
+lm.model.predito = data.frame(
+  CD_FAIXA_ETARIA = tst$CD_FAIXA_ETARIA, 
+  Premio_Puro = lm.model.predito) %>% 
+  group_by(CD_FAIXA_ETARIA) %>% 
+  summarise(Premio_Puro = mean(Premio_Puro)) %>% 
+  data.frame()
 
+ggplot() +
+  geom_point(aes(x = 1:10, y = produto_hipo.rf, col = "Random Forest")) +
+  geom_point(aes(x = 1:10, y = produto_hipo.dt, col = "Decision Tree")) +
+  scale_fill_manual(values = c(`Random Forest` = "red",
+                               `Decision Tree` = "blue"))
 
+produto_hipo.rf
+produto_hipo.dt
 
+ggplot() +
+  geom_point(aes(x = 1:10, y = lm.model.rf.predito, col = "Random Forest")) +
+  geom_point(aes(x = 1:10, y = lm.model.dt.predito, col = "Decision Tree")) +
+  scale_fill_manual(values = c(`Random Forest` = "red",
+                               `Decision Tree` = "blue"))
+
+lm.model.rf.predito
+lm.model.dt.predito
+
+cat(
+  "         VERIFICANDO AS VARIAÇÕES ENTRE AS FAIXAS ETÁRIAS \n                 Modelo por Random Forest",
+  "\nVariação entre a 10º e a 1º Faixa Etária: ",
+  round(lm.model.rf.predito[10] / lm.model.rf.predito[1], 2),
+  "\nVariação entre a 10º e a 7º Faixa Etária: ",
+  round(lm.model.rf.predito[10] / lm.model.rf.predito[7], 2),
+  "\nVariação entre a 7º e a 1º Faixa Etária: ",
+  round(lm.model.rf.predito[7] / lm.model.rf.predito[1], 2)
+)
+
+cat(
+  "         VERIFICANDO AS VARIAÇÕES ENTRE AS FAIXAS ETÁRIAS \n                 Modelo por Decision Tree",
+  "\nVariação entre a 10º e a 1º Faixa Etária: ",
+  round(lm.model.dt.predito[10] / lm.model.dt.predito[1], 2),
+  "\nVariação entre a 10º e a 7º Faixa Etária: ",
+  round(lm.model.dt.predito[10] / lm.model.dt.predito[7], 2),
+  "\nVariação entre a 7º e a 1º Faixa Etária: ",
+  round(lm.model.dt.predito[7] / lm.model.dt.predito[1], 2)
+)
